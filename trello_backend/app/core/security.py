32067df84f116f2f80a1ab85d73d49ec803339f9
Viraj Bhade <viraj.bhade1@gmail.com>
2026-05-22 12:50:54 +0530

    Added environment variable support

--- trello_backend/app/core/security.py
+++ trello_backend/app/core/security.py
@@ -1,48 +1,76 @@
 import bcrypt
+import os
+
+from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from jose import jwt
 
+# LOAD .env FILE
 
-SECRET_KEY = "your_secret_key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+load_dotenv()
+
+# READ VARIABLES FROM .env
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+)
+
+# PASSWORD HASHING
 
 def hash_password(password: str) -> str:
-    
-    pwd_bytes = password.encode('utf-8')
+
+    pwd_bytes = password.encode("utf-8")
+
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(pwd_bytes, salt)
-   
-    return hashed_password.decode('utf-8')
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    password_bytes = plain_password.encode('utf-8')
-    hashed_password_bytes = hashed_password.encode('utf-8')
-    return bcrypt.checkpw(password_bytes, hashed_password_bytes)
+    hashed_password = bcrypt.hashpw(
+        pwd_bytes,
+        salt
+    )
 
-from jose import jwt
-from datetime import datetime, timedelta
+    return hashed_password.decode("utf-8")
 
-SECRET_KEY = "your_secret_key"
-ALGORITHM = "HS256"
 
-from jose import jwt
-from datetime import datetime, timedelta
+def verify_password(
+    plain_password: str,
+    hashed_password: str
+) -> bool:
 
-SECRET_KEY = "your_secret_key"
-ALGORITHM = "HS256"
+    password_bytes = plain_password.encode("utf-8")
+
+    hashed_password_bytes = hashed_password.encode("utf-8")
+
+    return bcrypt.checkpw(
+        password_bytes,
+        hashed_password_bytes
+    )
+
+# CREATE JWT TOKEN
+
 def create_access_token(data: dict):
-    
+
     to_encode = data.copy()
-    
-   
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    
-    
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
     to_encode.update({"exp": expire})
-    
-   
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+# DECODE JWT TOKEN
 
 def decode_access_token(token: str):
-    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+    return jwt.decode(
+        token,
+        SECRET_KEY,
+        algorithms=[ALGORITHM]
+    )
